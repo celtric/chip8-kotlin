0c597bdedea54a368694491a0ed345086f82ffc3
Ricard Osorio <celtric@gmail.com>
2018-12-22 23:22:41 +0100

    Debug first instruction

--- src/main/kotlin/org/celtric/experiments/chip8/app.kt
+++ src/main/kotlin/org/celtric/experiments/chip8/app.kt
@@ -11,6 +11,7 @@ class App {
     fun run(filename: String) {
         val memory = Memory()
         memory.load(ROM(filename))
+        memory.debug()
     }
 }
 
@@ -25,8 +26,30 @@ class Memory {
         val romContent = rom.read()
         System.arraycopy(romContent, 0, state, firstPublicLocation, romContent.size)
     }
+
+    fun debug() {
+        opcode(firstPublicLocation).debug()
+    }
+
+    private fun opcode(address: Int) = Opcode(state[address], state[address + 1])
 }
 
 class ROM(private val filename: String) {
     fun read() = DataInputStream(BufferedInputStream(javaClass.getResourceAsStream(filename))).use { it.readBytes() }
 }
+
+class Opcode(private val mostSignificantByte: Byte, private val leastSignificantByte: Byte) {
+
+    fun debug() {
+        val upperNibble = mostSignificantByte.upperNibble()
+
+        when (upperNibble) {
+            0x6 -> println("Store 0x${leastSignificantByte.toHex()} in register v${mostSignificantByte.lowerNibble()}")
+            else -> println("Unknown instruction $upperNibble")
+        }
+    }
+}
+
+fun Byte.upperNibble() = (toInt() and 0xf0) shr 4
+fun Byte.lowerNibble() = toInt() and 0xf
+fun Byte.toHex() = Integer.toHexString(toInt())
